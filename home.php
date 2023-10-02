ff7015421db206698cfd9432bc008c20b1254f2b
Gabrieltbernardes <146398493+Gabrieltbernardes@users.noreply.github.com>
2023-10-02 10:42:27 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/35329e839e.js" crossorigin="anonymous"></script>
+
+    <!-- Estilo customizado -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Finans - finanças pessoais</title>
+  </head>
+  <body>
+    
+    <header><!-- inicio Cabecalho -->
+      <nav  class="navbar navbar-expand-sm navbar-light">
+        
+        <div class="container">
+          
+          <a href="#" class="navbar-brand">
+           logo
+          </a>
+
+          <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-principal">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="nav-principal">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a href="" class="nav-link">Home</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Sobre nos</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Faq</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Agenda</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="btn btn-outline-dark ml-4">Orçamento</a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+    </header><!--/fim Cabecalho -->
+
+    <section><!--hero section-->
+        <main class="mt-2">
+            <div class="container">
+            <div id="caixa-conteudo" class="row " style="background-color: aliceblue;">
+                    <div class=" col-md-6 ">
+                      <div class="align-self-center m-3">
+                        <h1 class="display-4">Crie uma </br> Memoria </br><span style="color:blue;">Brincante</span></h1>
+          
+                        <p class="my-4" >Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                          Accusamus dolores magni soluta sapiente reiciendis tempora totam, 
+                          sed fugit consectetur sit similique consequuntur ducimus, mollitia 
+                          voluptatum? Autem iure et dolore o</p>
+                          
+                        <div class="container p-0">
+                          <a href="" class="btn btn-primary text-center my-1 px-4" style="color: white;">Veja mais <i class="fa-solid fa-caret-down d-block"></i></a>
+                          <p class="d-inline">Prova social</p>
+                          <span class=""></span>
+                        </div>
+                       
+                      </div>
+                      
+                    </div>
+                    <div class="col-md-3 offset-2 d-none d-md-block ">
+                     <img class="justify-content-end" src="../imagens/d.png" style="height: 500px;">
+                    </div>
+        </main>
+    </section><!--fim hero section-->
+
+    
+    <section class=" mb-5"><!--nossas propostas-->
+        <div class="container-fluid text-center mt-3 mb-5"><h1 class="display-4">MENU
+
+        <div class="container-fluid d-flex ">
+            <div class="row justify-content-center">
+
+          <div class="col-md-3" style=" border-radius: 25px;border: 3px solid #73AD21;padding: 25px;width: 100%;margin: 5px;height: auto;">
+                <h3 class="">Clique no botão abaixo para pesquisar a criança desejada.</h3>
+                <div class="justify-content-center d-flex">
+                <a><button class="btn btn-primary px-6 my-2">Pesquisa</button></a>
+                </div>
+            </div>
+
+            <div class="col-md-3" style=" border-radius: 25px;border: 3px solid #73AD21;padding: 25px;width: 100%;margin: 5px;height: auto;">
+                <h4 class="">Clique no botão abaixo para adicionar informações nutricionais de um paciente.</h4>
+                <div class="justify-content-center d-flex">
+                <a><button class="btn btn-primary px-6 my-2">Adicionar</button></a>
+                </div>
+            </div>
+
+            <div class="col-md-3" style=" border-radius: 25px;border: 3px solid #73AD21;padding: 25px;width: 100%;margin: 5px;height: auto;">
+                <h4 class="">Clique no botão abaixo para consultar as informações nutricionais de todos os pacientes.</h4>
+                <div class="justify-content-center d-flex">
+                <a><button class="btn btn-primary px-6 my-2">Consultar</button></a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section><!--fim MENU-->
+
+    <section>
+        <div>
+            <h1>conclusão </h1>
+        </div>
+    </section>
+  
+    
+    <footer class="mt-4 mb-4 " style="border-top: black 1px solid;">
+        <div class="container">
+          <div class="row">
+            <!--seesao
+            <div class="col-md-4">
+              <p>
+                <a class="m-1" href="">Home</a>
+                <a class="m-1" href="">Recursos</a>
+                <a class="m-1" href="">Benefícios</a>
+                <a class="m-1" href="">Preços</a>
+              </p>
+            </div>-->
+
+          
+            <div class="col d-flex justify-content-center mt-4">
+              <a href="" class="btn btn-outline-dark">
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="" class="btn btn-outline-dark ml-2">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="" class="btn btn-outline-dark ml-2">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="" class="btn btn-outline-dark ml-2">
+                <i class="fab fa-youtube"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+    </footer>
+  
+  
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html> 
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,83 @@
+<?php
+include('conexao.php');
+
+if(isset($_POST['email']) || isset($_POST['senha'])) {
+
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu e-mail";
+    } 
+    if(strlen($_POST['email']) == 0  AND strlen($_POST['senha']) == 0){
+        echo " \r\n e preencha sua senha";
+    }
+    else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
+
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) {
+            
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: painel.php");
+
+        } 
+        else if(strlen($_POST['email']) != 0 AND $quantidade != 1){
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+
+    }
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
+
+    <title>Bootstrap Login Form</title>
+  </head>
+  <body>
+    <section>
+      <div class="container mt-5 pt-5"><!--centralizar no meio-->
+        <div class="row">
+          <div class="col-12 col-sm-7 col-md-6 m-auto "><!--responsividade-->
+            <div class="card border-0 shadow" style="border-radius: 25px;"><!--sombra e canto redondo-->
+              <div class="card-body m-4"><!--tamanho da caixa-->
+                <h1 class="d-flex justify-content-center">Faça seu login</h1><!--centraliza o texto no meio-->
+                <form action="" method="POST">
+                  <input type="text" name="email" id="" class="form-control my-4 py-2" placeholder="Email" />
+                  <input type="password" name="senha" id="" class="form-control my-4 py-2" placeholder="Senha" />
+                  <div class="text-center mt-3">
+                    <button class="btn btn-primary">Login</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+  </body>
+</html>
